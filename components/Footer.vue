fd761140525f62e3cf36c779891f88e6b8a98b49
louweal <anneloeslouwe@live.nl>
2025-10-25 11:54:17 +0200

    add link target

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,8 +4,9 @@
             <div class="grid md:grid-cols-4"></div>
 
             <p class="text-center text-sm opacity-50">
-                © Copyright 2025 TruSense - Powered by <a href="https://hedera.com">Hedera</a> - Development:
-                <a href="https://github.com/louweal">louweal</a>
+                © Copyright 2025 TruSense - Powered by <a href="https://hedera.com" target="_blank">Hedera</a> -
+                Development:
+                <a href="https://github.com/louweal" target="_blank">louweal</a>
             </p>
         </div>
     </footer>
